2af05b58ead05cea40f4efb54242d375cc2d9dbd
myne145 <antoni.kucz@gmail.com>
2024-10-22 06:56:54 +0200

    fixed the code

--- src/main/java/com/myne145/quickParkourCheckpointCreator/MainCommand.java
+++ src/main/java/com/myne145/quickParkourCheckpointCreator/MainCommand.java
@@ -3,7 +3,9 @@ package com.myne145.quickParkourCheckpointCreator;
 import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jspecify.annotations.Nullable;
 
@@ -20,22 +22,26 @@ public class MainCommand implements BasicCommand {
         }
         if(strings.length == 2 && strings[0].equals("create")) {
             String courseName = strings[1];
+
+            if(!(commandSourceStack.getSender() instanceof Player)) {
+                return;
+            }
+
+            Player player = (Player) commandSourceStack.getSender();
+            GameMode prevGamemode = player.getGameMode();
+
             Bukkit.dispatchCommand(commandSourceStack.getSender(), "pa create checkpoint " + courseName);
-            Bukkit.dispatchCommand(commandSourceStack.getSender(), "gamemode spectator");
-            Bukkit.dispatchCommand(commandSourceStack.getSender(), "tp @p ~ ~1.5 ~");
-
-            new BukkitRunnable() {
-
-                @Override
-                public void run() {
-                    commandSourceStack.getSender().sendMessage("fsdkjkldsfpkjsdf");
-                }
-            }.runTaskLater(QuickParkourCheckpointCreator.plugin, 20);
+            player.setGameMode(GameMode.SPECTATOR);
+            player.teleport(player.getLocation().add(0, 1.5, 0));
 
             Bukkit.dispatchCommand(commandSourceStack.getSender(),
                     "dh create " + courseName + "_checkpoint_1 %parkour_current_checkpoint_hologram_" +
-                            courseName + "_ " + QuickParkourCheckpointCreator.checkpointCounter + "%"
+                            courseName + "_" + QuickParkourCheckpointCreator.checkpointCounter + "%"
             );
+
+            player.teleport(player.getLocation().add(0, -1.5, 0));
+            player.setGameMode(prevGamemode);
+
             QuickParkourCheckpointCreator.checkpointCounter++;
             commandSourceStack.getSender().sendMessage("Successfully executed all the commands");
             return;
